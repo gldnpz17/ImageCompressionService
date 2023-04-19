a8fc77362430267fbc39b6f6fef48f3a617c3977
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2023-04-19 22:05:49 +0700

    Fixed some issues related to headers.

--- imgcompress/src/main/java/com/gldnpz/imgcompress/ImgcompressApplication.java
+++ imgcompress/src/main/java/com/gldnpz/imgcompress/ImgcompressApplication.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
@@ -22,6 +19,7 @@ import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -38,10 +36,18 @@ public class ImgcompressApplication {
 
 	@GetMapping("/compress")
 	@ResponseBody
-	public ResponseEntity<InputStreamResource> compressImage(@RequestParam String url, @RequestParam Float quality) throws IOException {
-		URL imageUrl = new URL(url);
-		String mimeType = imageUrl.openConnection().getContentType();
-		BufferedImage image = ImageIO.read(imageUrl);
+	public ResponseEntity<InputStreamResource> compressImage(
+			@RequestParam String url,
+			@RequestParam Float quality,
+			@RequestHeader Map<String, String> headers
+	) throws IOException {
+		URLConnection imageConnection = new URL(url).openConnection();
+
+		for (Map.Entry<String, String> header : headers.entrySet()) {
+			imageConnection.setRequestProperty(header.getKey(), header.getValue());
+		}
+		String mimeType = imageConnection.getContentType();
+		BufferedImage image = ImageIO.read(imageConnection.getInputStream());
 
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
